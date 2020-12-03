9b27148bd3dc81b8fabcb328122fbccb884f4350
fionyy <xiaoyouding@163.com>
2020-12-03 13:49:43 +0800

    删除环境变量

--- auto/pipeline/database_test/test_autolab.groovy
+++ auto/pipeline/database_test/test_autolab.groovy
@@ -12,7 +12,7 @@ node {
     }
 
     stage("Test"){
-        powershell "pytest --version"
+        powershell.exe --command "pytest --version"
         dir("."){
             powershell "pwd"
             powershell "pytest ${testplan} --html=./logs/report.html --self-contained-html"
